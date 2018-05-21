192b2b3e50791c4da2b1daf1816ead11dfed4da5
Andrey Buchnev <mr.buchnev@gmail.com>
2018-05-21 10:19:32 +0000

    update google maps functionality

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,15 +3,14 @@
 var express     = require("express"),
     router      = express.Router(),
     Campground  = require("../models/campground"),
-    middleware  = require("../middleware"); //if it's a link to a directory and the file inside is named "index", then the system will automatically import that file
-
-var NodeGeocoder = require('node-geocoder');
+    middleware  = require("../middleware"), //if it's a link to a directory and the file inside is named "index", then the system will automatically import that file
+    NodeGeocoder = require('node-geocoder');
  
 var options = {
-    provider: 'google',
-    httpAdapter: 'https',
-    apiKey: process.env.GEOCODER_API_KEY,
-    formatter: null
+  provider: 'google',
+  httpAdapter: 'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null
 };
  
 var geocoder = NodeGeocoder(options);
@@ -33,18 +32,15 @@ router.post("/", middleware.isLoggedIn, function(req, res){
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
+  var cost = req.body.cost;
   var author = {
       id: req.user._id,
       username: req.user.username
-  }
-  var cost = req.body.cost;
+  };
   geocoder.geocode(req.body.location, function (err, data) {
     if (err || !data.length) {
-        console.log(err);
-        console.log("===============");
-        console.log(data);
-        req.flash('error', 'Invalid address');
-        return res.redirect('back');
+      req.flash('error', 'Invalid address');
+      return res.redirect('back');
     }
     var lat = data[0].latitude;
     var lng = data[0].longitude;
@@ -56,6 +52,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
             console.log(err);
         } else {
             //redirect back to campgrounds page
+            console.log(newlyCreated);
             res.redirect("/campgrounds");
         }
     });
@@ -97,15 +94,14 @@ router.get("/:id/edit", middleware.checkCampgroundOwnership, function(req, res){
 router.put("/:id", middleware.checkCampgroundOwnership, function(req, res){
   geocoder.geocode(req.body.location, function (err, data) {
     if (err || !data.length) {
-        console.log(err);
-        req.flash('error', 'Invalid address');
-        return res.redirect('back');
+      req.flash('error', 'Invalid address');
+      return res.redirect('back');
     }
-    var lat = data[0].latitude;
-    var lng = data[0].longitude;
-    var location = data[0].formattedAddress;
-    var newData = {name: req.body.campground.name, image: req.body.campground.image, description: req.body.campground.description, cost: req.body.campground.cost, location: location, lat: lat, lng: lng};
-    Campground.findByIdAndUpdate(req.params.id, newData, function(err, campground){
+    req.body.campground.lat = data[0].latitude;
+    req.body.campground.lng = data[0].longitude;
+    req.body.campground.location = data[0].formattedAddress;
+
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, campground){
         if(err){
             req.flash("error", err.message);
             res.redirect("back");
